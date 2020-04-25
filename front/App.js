46b955414aaa01a9e294184038cf31c0cefd5277
joseiciano <icianojose12345@aol.com>
2020-04-25 15:27:58 -0400

    got camera setup, now working on qr scanner

--- front/App.js
+++ front/App.js
@@ -1,74 +1,127 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+/* eslint-disable no-console */
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  TouchableOpacity,
+  Slider,
+  TouchableWithoutFeedback,
+  Dimensions,
+  Button,
 } from 'react-native';
+// eslint-disable-next-line import/no-unresolved
+import {RNCamera} from 'react-native-camera';
+import CameraScreen from './src/components/CameraScreen';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App = () => {
-  return (
-    <SafeAreaView>
-      <Text>Pepega</Text>
-    </SafeAreaView>
-  );
+const flashModeOrder = {
+  off: 'on',
+  on: 'auto',
+  auto: 'torch',
+  torch: 'off',
 };
 
+const wbOrder = {
+  auto: 'sunny',
+  sunny: 'cloudy',
+  cloudy: 'shadow',
+  shadow: 'fluorescent',
+  fluorescent: 'incandescent',
+  incandescent: 'auto',
+};
+
+const landmarkSize = 2;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderCamera: false,
+    };
+  }
+
+  toggleCamera = () => this.setState({renderCamera: !this.state.renderCamera});
+  render() {
+    if (this.state.renderCamera) {
+      return <CameraScreen toggle={this.toggleCamera} />;
+    } else {
+      return (
+        <View>
+          <Text>Pepega</Text>
+          <Button title="Pepega" onPress={this.toggleCamera.bind(this)} />
+          {/* {this.state.renderCamera && <CameraScreen />} */}
+        </View>
+      );
+    }
+  }
+}
+
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    paddingTop: 10,
+    // backgroundColor: '#000',
   },
-  engine: {
+  flipButton: {
+    flex: 0.3,
+    height: 40,
+    marginHorizontal: 2,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 8,
+    borderColor: 'white',
+    borderWidth: 1,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  autoFocusBox: {
     position: 'absolute',
+    height: 64,
+    width: 64,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: 'white',
+    opacity: 0.4,
+  },
+  flipText: {
+    color: 'white',
+    fontSize: 15,
+  },
+  zoomText: {
+    position: 'absolute',
+    bottom: 70,
+    zIndex: 2,
+    left: 2,
+  },
+  picButton: {
+    backgroundColor: 'darkseagreen',
+  },
+  facesContainer: {
+    position: 'absolute',
+    bottom: 0,
     right: 0,
+    left: 0,
+    top: 0,
   },
-  body: {
-    backgroundColor: Colors.white,
+  landmark: {
+    width: landmarkSize,
+    height: landmarkSize,
+    position: 'absolute',
+    backgroundColor: 'red',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  text: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 2,
+    position: 'absolute',
+    borderColor: '#F00',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  textBlock: {
+    color: '#F00',
+    position: 'absolute',
+    textAlign: 'center',
+    backgroundColor: 'transparent',
   },
 });
-
-export default App;
