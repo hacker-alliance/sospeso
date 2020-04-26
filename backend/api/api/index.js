f6e2baeeefb5d91c0b1966307bd5372e02a2303b
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-04-26 02:42:10 -0400

    Add Accounts

--- backend/api/api/index.js
+++ backend/api/api/index.js
@@ -3,61 +3,108 @@ const express = require('express');
 
 const Cloudant = require('@cloudant/cloudant');
 const cloudant = Cloudant(process.env.cloudant_url);
-const db = cloudant.db.use('sospeso')
+const authdb = cloudant.db.use('authentication');
+const vendordb = cloudant.db.use('vendor');
 
 let app = express();
 
-function defaultRoute(req, res) {
-    res.send(req.path);
-}
-async function getAccounts() {
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+function defaultRoute(req, res) {
+    let response = {
+        'path': req.path,
+        'body': req.body
+    }
+    res.send(response);
+}
+
+async function getAccounts() {
+    let response = {};
+    let records = await authdb.list();
+    console.log(records.rows);
+    response.accounts = records.rows;
+    return response;
 }
 
 async function createAccount(req) {
-
-}
-
-async function accounts(req, res) {
     let response = {};
-    switch (req.method) {
-        case 'GET':
-            response = await getAccounts();
-        case 'POST':
-            response = await createAccount(req);
-    }
-    res.send(response);
+
+    let record = await authdb.insert({
+        _id: req.body.accountID,
+
+    });
+    console.log(record);
+    response.accountID = record.id;
+    return response;
 }
 
-async function getAccount(req) {
-
-}
-async function authenticateAccount(req) {
-
-}
 async function updateAccount(req) {
+    let response = {};
+    let findRecord = await authdb.get(req.body.accountID);
+    console.log(findRecord);
+    findRecord.test = 'test';
+    let record = await authdb.insert(findRecord);
 
+    return response;
 }
 
 async function deleteAccount(req) {
+    let response = {};
+    let findRecord = await authdb.get(req.body.accountID);
+    console.log(findRecord);
+    let record = await authdb.destroy(
+        req.body.accountID,
+        findRecord._rev
+    );
 
+    console.log(record);
+    response.accountID = record.id;
+    return response;
 }
 
-async function account(req, res) {
-    let response = {}
+async function accounts(req, res) {
+    let responsePromise = {};
     switch (req.method) {
         case 'GET':
-            response = await getAccount(req);
+            responsePromise = getAccounts(req);
+            break;
         case 'POST':
-            response = await authenticateAccount(req);
+            responsePromise = createAccount(req);
+            break;
         case 'PUT':
-            response = await updateAccount(req);
+            responsePromise = updateAccount(req);
+            break;
         case 'DELETE':
-            response = await deleteAccount(req);
+            responsePromise = deleteAccount(req);
     }
+    response = await responsePromise.catch(
+        error => res.status(500).send({ error })
+    );
     res.send(response);
 }
 
+async function authenticateAccount(req) {
+    let response = {};
+    let record = await authdb.get(req.body.accountID);
+    console.log(record);
+    response.accountID = record._id;
+    console.log(response);
+    return response;
+}
+
+async function authenticate(req, res) {
+    let responsePromise = {};
+    switch (req.method) {
+        case 'POST':
+            responsePromise = authenticateAccount(req)
+            break;
+    }
+    response = await responsePromise.catch(
+        error => res.status(500).send({ error })
+    );
+    res.send(response);
+}
 async function getVendors() {
     let response = {};
     response.found = await db.list();
@@ -69,15 +116,17 @@ async function createVendor(req) {
 }
 
 async function vendors(req, res) {
-    let response = {};
+    let responsePromise = {};
     switch (req.method) {
         case 'GET':
-            response = await getVendors();
+            responsePromise = getVendors();
+            break;
         case 'POST':
-            reponse = await createVendor(req);
+            reponse = createVendor(req);
+            break;
     }
 
-    res.send(response);
+    res.send(responsePromise);
 }
 
 async function getVendor(req) {
@@ -97,10 +146,13 @@ async function vendor(req, res) {
     switch (req.method) {
         case 'GET':
             response = await getVendor(req);
+            break;
         case 'PUT':
             response = await updateVendor(req);
+            break;
         case 'DELETE':
             response = await deleteVendor(req);
+            break;
     }
 
     res.send(response);
@@ -119,8 +171,10 @@ async function items(req, res) {
     switch (req.method) {
         case 'GET':
             response = await getItems();
+            break;
         case 'POST':
             response = await createItem(req);
+            break;
     }
 
     res.send(response);
@@ -146,18 +200,23 @@ async function item(req, res) {
     switch (req.method) {
         case 'GET':
             response = await getItem(req);
+            break;
         case 'POST':
             response = await validateItem(req);
+            break;
         case 'PUT':
             response = await updateItem(req);
+            break;
         case 'DELETE':
             response = await deleteItem(req);
+            break;
     }
 
     res.send(response);
 }
+
+app.use('/account/authenticate', authenticate);
 app.use('/account', accounts);
-app.use('/account/:accountID', account);
 app.use('/vendor', vendors);
 app.use('/vendor/:vendor', vendor);
 app.use('/vendor/:vendorID/item', items);
